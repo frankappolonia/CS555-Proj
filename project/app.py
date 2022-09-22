cc3bae3c9b620221d9906ca756ad31e212d7f9f8
James Lepore <jlepore@stevens.edu>
2022-09-22 13:13:59 -0400

    Simple storage of individuals and families

--- project/app.py
+++ project/app.py
@@ -1,8 +1,9 @@
 import os
+from pprint import pprint
 working_directory = os.getcwd()
 file_path = working_directory + "myFamily.ged"
 
-f = open(working_directory + "\project\myFamily.ged")
+f = open(working_directory + "/myFamily.ged")
 results = open("output.txt", "w")
 
 
@@ -56,5 +57,58 @@ for line in f:
 
     results.write(output + "\n")
 
+f.close()
 results.close()
 
+
+### Store the results in data structures ###
+individuals = {}
+families = {}
+
+parsing_indi = False
+parsing_fam = False
+curr_name = ""
+curr = {}
+with open("output.txt", 'r') as filehandle:
+    for line in filehandle:
+        if "<--" in line:
+            print(line)
+            fields = line.strip("<-- ").strip("\n").split("|")
+            # case 1: let's start parsing a new individual or family
+            if fields[0] == '0':
+                # first, save any previous info
+                if curr != {} and parsing_fam:
+                    families[curr_name] = curr
+                    curr = {}
+                    curr_name = ""
+                    parsing_fam = False
+                elif curr != {} and parsing_indi:
+                    individuals[curr_name] = curr
+                    curr = {}
+                    curr_name = ""
+                    parsing_indi = False
+                # then, set up the new info
+                if fields[1] == "INDI":
+                    parsing_indi = True
+                    curr_name = fields[3]
+                elif fields[1] == "FAM":
+                    parsing_fam = True
+                    curr_name = fields[3]
+            # case 2: let's add info to the individual or family
+            elif (parsing_fam or parsing_indi) and fields[2] == "Y":
+                curr[fields[1]] = fields[3]
+
+# Add in the last added info             
+if curr != {} and parsing_fam:
+    families[curr_name] = curr
+    curr = {}
+    curr_name = ""
+    parsing_fam = False
+elif curr != {} and parsing_indi:
+    individuals[curr_name] = curr
+    curr = {}
+    curr_name = ""
+    parsing_indi = False
+    
+pprint(families)
+pprint(individuals)
